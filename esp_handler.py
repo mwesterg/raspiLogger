f66b47de6ec8ef79ae8390ef8dbb77f07d9a9a73
Martin Westermeyer <mwestermeyer@udec.cl>
2025-08-18 16:20:12 -0400

    .

--- esp_handler.py
+++ esp_handler.py
@@ -195,15 +195,15 @@ class ESPManager:
             target_offset = 0x10000
 
             logging.info(f"Flashing {firmware_path} to partition '{partition_name}' at offset 0x{target_offset:x} on {port}...")
-            
-            write_flash(
-            esp=self.esp_global,
-            args=None,   # CLI args object is optional here
-            address_filename=[(target_offset, firmware_path)],
-            flash_size="detect",
-            no_progress=False,
-            encrypt=False
-            )
+            with open(firmware_path,"rb") as bin_file:
+                write_flash(
+                    esp=self.esp_global,
+                    args=None,   # CLI args object is optional here
+                    address_filename=[(target_offset, bin_file)],
+                    flash_size="detect",
+                    no_progress=False,
+                    encrypt=False
+                )
             self.reset_esp32(port, get_info=True)
                 
             logging.info(f"Flashing complete.")
