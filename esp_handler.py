3587d4b6346eb218f37470bdaf09b718550c9dce
Martin Westermeyer <mwestermeyer@udec.cl>
2025-08-18 16:29:40 -0400

    .

--- esp_handler.py
+++ esp_handler.py
@@ -195,7 +195,7 @@ class ESPManager:
             target_offset = 0x10000
 
             logging.info(f"Flashing {firmware_path} to partition '{partition_name}' at offset 0x{target_offset:x} on {port}...")
-            
+            reset_chip(self.esp_global, reset_mode="default-reset")
             with open(firmware_path,"rb") as bin_file:
                 write_flash(
                     self.esp_global,
